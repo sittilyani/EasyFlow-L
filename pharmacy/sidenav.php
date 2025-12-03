3a6bd96455dfeb5bfb8d31bc1c5ce58b2efe0673
Lyani Sitti <33978205+sittilyani@users.noreply.github.com>
2025-12-03 07:03:29 +0300

    update doses

--- pharmacy/sidenav.php
+++ pharmacy/sidenav.php
@@ -58,7 +58,7 @@ $user_id = $_SESSION['user_id'] ?? '';
                 <a href="../pharmacy/retro_dispensing_module.php" target="contentFrame" class="nav-link">
                     <i class="fa fa-anchor"></i>Retro Dispensing</a>
                 <a href="../pharmacy/edit_dispensed_dose.php" target="contentFrame" class="nav-link" style="background: #ccccff; color: #000000; margin-top: 10px;">
-                    <i class="fa fa-anchor"></i>Delete dispensed doses</a>
+                    <i class="fa fa-anchor"></i>Edit dispensed doses</a>
                 <a href="../pharmacy/inventory_form.php" target="contentFrame" class="nav-link" style="background: #b1f0c2; color: #000000; margin-top: 10px;">
                     <i class="fa fa-anchor"></i>Daily Stores movements</a>
                 <a href="../pharmacy/add_stocks.php" target="contentFrame" class="nav-link">
